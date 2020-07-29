bcd6edebe38ea81b823202f3b17b8ca94a975776
Sabrina Colman <sabrina-rct@hotmail.com>
2020-07-29 11:24:55 -0300

    transcribi las variables a ingles

--- exercise-twelve/src/MyApp.java
+++ exercise-twelve/src/MyApp.java
@@ -4,46 +4,48 @@ public class MyApp {
 
 
     public static void main(String[] args) {
+//        Una empresa desea determinar el monto de un cheque que debe proporcionar a uno de
+//        sus empleados que tendrá que ir por equis número de días a la ciudad de Monterrey; los
+//        gastos que cubre la empresa son: hotel, comida y 100.00 pesos diarios para otros
+//        gastos. El monto debe estar desglosado para cada concepto. Realice un diagrama de
+//        flujo y pseudocódigo que representen el algoritmo que determine el monto del cheque.
+
         //declarar las variables y darle nombre al programa
 
         System.out.println("Programa para determinar el monto de un cheque");
         System.out.println("**********************************************");
 
-        int numdias;
-        int montohotel;
-        int montocomi;
-        final int costodiario = 100;
-        int montodiario;
+        int daysnumber;
+        int hotelcost;
+        int foodcost;
+        final int priceperday = 100;
+        int amountperday;
         int th;
-        int tc;
-        int t;
+        int tf;
+        int total;
 
         //metodo para ingrersar datos por teclado
 
         Scanner keyboard = new Scanner(System.in);
 
         System.out.println("Ingrese numero de dias");
-        numdias = keyboard.nextInt();
+        daysnumber = keyboard.nextInt();
         System.out.println("Ingrese monto de hotel");
-        montohotel = keyboard.nextInt()
+        hotelcost = keyboard.nextInt()
         System.out.println("INgrese monto de comida");
-        montocomi = keyboard.nextInt();
+        foodcost = keyboard.nextInt();
 
 
         // hacer proceso
 
-        montodiario = numdias * costodiario;
-        th = numdias * montohotel;
-        tc = numdias * montocomi;
-        t = montodiario + th + tc;
+        amountperday = daysnumber * priceperday;
+        th = daysnumber * hotelcost;
+        tf = daysnumber * foodcost;
+        total = amountperday + th + tf;
 
         // mostrar resultado
 
-        System.out.println("El monto del cheque es de: " + t);
-
-
-
-
+        System.out.println("El monto del cheque es de: " + total);
 
 
     }
