90071af44b7d3ed34600083d93a4fd5f38ff1634
Sabrina Colman <sabrina-rct@hotmail.com>
2020-07-29 21:17:19 -0300

    modifique tipo de variables y utilice el formato printf() cadena formato

--- exercise-twelve/src/MyApp.java
+++ exercise-twelve/src/MyApp.java
@@ -15,38 +15,40 @@ public class MyApp {
         System.out.println("Programa para determinar el monto de un cheque");
         System.out.println("**********************************************");
 
-        int daysnumber;
-        int hotelcost;
-        int foodcost;
+        int daysNumber;
+        double hotelcost;
+        double foodcost;
         final int priceperday = 100;
-        int amountperday;
-        int th;
-        int tf;
-        int total;
+        double amountperday;
+        double totalHotel;
+        double totalFood;
+        double total;
 
         //metodo para ingrersar datos por teclado
 
         Scanner keyboard = new Scanner(System.in);
 
         System.out.println("Ingrese numero de dias");
-        daysnumber = keyboard.nextInt();
+        daysNumber = keyboard.nextInt();
         System.out.println("Ingrese monto de hotel");
-        hotelcost = keyboard.nextInt()
-        System.out.println("INgrese monto de comida");
-        foodcost = keyboard.nextInt();
+        hotelcost = keyboard.nextDouble();
+        System.out.println("Ingrese monto de comida");
+        foodcost = keyboard.nextDouble();
 
 
         // hacer proceso
 
-        amountperday = daysnumber * priceperday;
-        th = daysnumber * hotelcost;
-        tf = daysnumber * foodcost;
-        total = amountperday + th + tf;
+        amountperday = daysNumber * priceperday;
+        totalHotel = daysNumber * hotelcost;
+        totalFood = daysNumber * foodcost;
+        total = amountperday + totalHotel + totalFood;
 
         // mostrar resultado
-
-        System.out.println("El monto del cheque es de: " + total);
-
+        //System.out.println("El monto del hotel: " + totalHotel);
+        //System.out.println("El monto de comida: " + totalFood);
+        //System.out.println("El monto del cheque es de: " + total);
+        //metodo para concatenar variables %d es para variables int, si es con double %5.2f(2 indica cantidad de decimales)
+        System.out.printf("Los montos son: \nHotel $ %5.2f \nComida $ %5.2f \nTotal $ %5.2f", totalHotel, totalFood, total);
 
     }
 }
